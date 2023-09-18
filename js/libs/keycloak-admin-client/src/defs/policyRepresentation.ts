4ee2f0e07e02d8ac03cd57d42fffdf4d193616e8
PhilKes <philk97@web.de>
2023-09-18 15:47:10 +0200

    Add clients field to PolicyRepresentation in JavaScript admin client

--- js/libs/keycloak-admin-client/src/defs/policyRepresentation.ts
+++ js/libs/keycloak-admin-client/src/defs/policyRepresentation.ts
@@ -24,6 +24,7 @@ export interface PolicyRoleRepresentation {
 }
 
 export default interface PolicyRepresentation {
+  clients?: string[];
   config?: Record<string, any>;
   decisionStrategy?: DecisionStrategy;
   description?: string;
